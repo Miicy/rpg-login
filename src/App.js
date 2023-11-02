6b00733a9ca9adf69757b67d1b719dab0779a5cc
Micy <100793297+Miicy@users.noreply.github.com>
2023-11-03 00:20:31 +0100

    fav icon updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import "./App.css";
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import { themeCreation } from "./theme/themeDesign";
+
 import Home from "./pages/Home";
 
 function App() {
-	const [theme] = useState(themeCreation());
-
 	return (
-		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-				</Routes>
-				{/* <LoadingModal />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+			{/* <LoadingModal />
 				<NotificationContainer /> */}
-			</BrowserRouter>
-		</ThemeProvider>
+		</BrowserRouter>
 	);
 }
 
