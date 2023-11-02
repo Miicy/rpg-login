4d5f79fc54c8b8b9aa79f401a9ffc8eb8983e0f2
Micy <100793297+Miicy@users.noreply.github.com>
2023-11-02 23:45:45 +0100

    portfolio work

---
+++ src/components/layout/AboutMe.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { ReactComponent as Divider } from "../../media/divider.svg";
+import text from "../../media/text.json";
+import wordFile from "../../media/CV.pdf";
+import { useState } from "react";
+
+function AboutMe() {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          setScrollPosition(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+    
+      useEffect(() => {
+        const scrollToShowAboutMe = 200; 
+        const aboutMeContainer = document.getElementById('about-me-container');
+        if (aboutMeContainer) {
+          if (scrollPosition >= scrollToShowAboutMe) {
+            aboutMeContainer.style.display = 'flex';
+          } else {
+            aboutMeContainer.style.display = 'none';
+          }
+        }
+      }, [scrollPosition]);
+
+	return (
+		<div id="about-me-container" className="about-me-container" >
+			<div className="about-me">
+				<Divider className="divider" />
+				<div className="about-me-content-container">
+					<p>{text.text}</p>
+					<a href={wordFile} download="Resume.pdf" className="links resume" >
+						Download Resume
+					</a>
+				</div>
+				<Divider className="divider divider-bottom" />
+			</div>
+		</div>
+	);
+}
+
+export default AboutMe;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "../../pages/pages.css";
 function Header() {
+	function scrollToPosition(yPosition) {
+		window.scrollTo({
+		  top: yPosition,
+		  behavior: "smooth",
+		});
+	  }
+	
+	  function handleAboutMeClick() {
+		const aboutMeSectionY = 800; 
+	
+		scrollToPosition(aboutMeSectionY);
+	  }
+
+	  function handlePortfolioClick() {
+		const aboutMeSectionY = 1800; 
+	
+		scrollToPosition(aboutMeSectionY);
+	  }
+
 	return (
 		<div class="header">
-			<p className="heading">Milica Pantelic</p>
-            <p className="heading2">Web developer</p>
+			<div class="heading-left-container">
+				<p className="heading">Milica Pantelic</p>
+				<p className="heading2">Web developer</p>
+			</div>
+			<div class="heading-right-container">
+				<p className="links" onClick={handleAboutMeClick}>
+					About me
+				</p>
+				<p className="links"  onClick={handlePortfolioClick}>Portfolio</p>
+			</div>
 		</div>
 	);
 }
