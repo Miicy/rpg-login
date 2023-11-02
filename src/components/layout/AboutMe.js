55663163782b795dcf15e5d59bb417d1a851a08e
Micy <100793297+Miicy@users.noreply.github.com>
2023-11-02 23:55:28 +0100

    fixed about me

--- src/components/layout/AboutMe.js
+++ src/components/layout/AboutMe.js
@@ -5,38 +5,39 @@ import wordFile from "../../media/CV.pdf";
 import { useState } from "react";
 
 function AboutMe() {
-    const [scrollPosition, setScrollPosition] = useState(0);
+	const [scrollPosition, setScrollPosition] = useState(0);
 
-    useEffect(() => {
-        const handleScroll = () => {
-          setScrollPosition(window.scrollY);
-        };
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrollPosition(window.scrollY);
+		};
 
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
-    
-      useEffect(() => {
-        const scrollToShowAboutMe = 200; 
-        const aboutMeContainer = document.getElementById('about-me-container');
-        if (aboutMeContainer) {
-          if (scrollPosition >= scrollToShowAboutMe) {
-            aboutMeContainer.style.display = 'flex';
-          } else {
-            aboutMeContainer.style.display = 'none';
-          }
-        }
-      }, [scrollPosition]);
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	useEffect(() => {
+		const scrollToShowAboutMe = 200;
+		const aboutMeContainer = document.getElementById("about-me-container");
+		if (aboutMeContainer) {
+			if (scrollPosition >= scrollToShowAboutMe) {
+				aboutMeContainer.style.display = "flex";
+			} else {
+				aboutMeContainer.style.display = "none";
+			}
+		}
+	}, [scrollPosition]);
 
 	return (
-		<div id="about-me-container" className="about-me-container" >
+		<div id="about-me-container" className="about-me-container">
 			<div className="about-me">
 				<Divider className="divider" />
 				<div className="about-me-content-container">
+					<p className="heading-smaller">ABOUT ME</p>
 					<p>{text.text}</p>
-					<a href={wordFile} download="Resume.pdf" className="links resume" >
+					<a href={wordFile} download="Resume.pdf" className="links resume">
 						Download Resume
 					</a>
 				</div>
