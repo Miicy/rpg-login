964f05f6389d6044e67633a10d178d708e4b6bbe
Micy <100793297+Miicy@users.noreply.github.com>
2023-11-03 01:57:26 +0100

    added footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,20 @@
+import { Divider } from "@mui/material";
 import React from "react";
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="footer">
+			<div className="inner-footer">
+				<p className="links">Git</p>
+				<Divider  orientation="vertical" flexItem sx={{border:"0.5px solid black",}}/>
+				<p className="links">Discord</p>
+				<Divider orientation="vertical" flexItem sx={{border:"0.5px solid black",}}/>
+				<p className="links">Phone</p>
+				<Divider orientation="vertical" flexItem sx={{border:"0.5px solid black",}}/>
+				<p className="links">Gmail</p>
+			</div>
+		</div>
+	);
 }
 
 export default Footer;
